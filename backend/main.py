22a987f895da185cc05d409d3c9a46c2ee05babb
juniordelorbe75-rgb <juniordelorbe75@gmail.com>
2026-08-05 14:45:07 -0700

    Add gitignore and update property endpoint

--- backend/main.py
+++ backend/main.py
@@ -28,3 +28,13 @@ def get_property(property_id: int):
 def create_property(property: Property):
     properties.append(property)
     return property
+
+
+@app.put("/properties/{property_id}")
+def update_property(property_id: int, updated_property: Property):
+    for index, property in enumerate(properties):
+        if property.id == property_id:
+            properties[index] = updated_property
+            return updated_property
+
+    return {"message": "Property not found"}
